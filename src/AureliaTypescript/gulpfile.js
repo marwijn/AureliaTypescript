079a73e915222c67af41e012f6f94b24b9acfffb
marwijn <marwijn.christiaans@gmail.com>
2015-08-07 16:51:16 +0200

    Minimal Aurelia Typescript

---
+++ src/AureliaTypescript/gulpfile.js
@@ -0,0 +1,13 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var notify = require('gulp-notify');
+
+gulp.task('copy-typings', function () {
+    return gulp.src('wwwroot/jspm_packages/**/*.d.ts')
+    .pipe(gulp.dest('scripts/typings/jspm_packages/'));
+});
+
---
+++ src/AureliaTypescript/scripts/app.ts
@@ -0,0 +1,4 @@
+﻿export class App
+{
+    name = "Aurelia";
+}
