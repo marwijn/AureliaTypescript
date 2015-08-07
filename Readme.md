e2d975fea89eace20a218d253bd9cfd2bc825a3e
marwijn <marwijn.christiaans@gmail.com>
2015-08-07 19:32:51 +0200

    Add readme, fix npm install

---
+++ Readme.md
@@ -0,0 +1,4 @@
+To compile the project:
+* run npm install from src\AureliaTypescript
+* run jspm install from src\AureliaTypescript
+* In visual studio use the taskrunner to run the copy_typings task
--- src/AureliaTypescript/gulpfile.js
+++ src/AureliaTypescript/gulpfile.js
@@ -4,7 +4,6 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
-var notify = require('gulp-notify');
 
 gulp.task('copy-typings', function () {
     return gulp.src('wwwroot/jspm_packages/**/*.d.ts')
